1b4b835b11c29cec2d77759e2e99ae0d066a898b
redperov <redperov@gmail.com>
2018-05-16 23:24:13 +0300

    Working network

--- ex_3.py
+++ ex_3.py
@@ -4,7 +4,7 @@ import pickle
 
 def main():
     # TODO uncomment this when submitting
-    # train_x = np.loadtxt("train_x")
+    # train_x = np.loadtxt("train_x") / 255.0
     # train_y = np.loadtxt("train_y")
     # test_x = np.loadtxt("test_x")
 
@@ -13,7 +13,8 @@ def main():
     data = pickle.loads(pickle_in)
     train_x = np.array(data[0]) / 255.0
     train_y = np.array(data[1])
-    test_x = np.array(data[2])
+    # TODO maybe divide by 255. as well
+    test_x = np.array(data[2]) / 255.0
 
     # Combine examples and labels into tuples.
     paired_examples = combine_examples(train_x, train_y)
@@ -26,14 +27,43 @@ def main():
     validation_set = paired_examples[-validation_size:]
     training_set = paired_examples[:-validation_size]
 
-    # Generate initial weights.
-    params = generate_weights()
-
     # Initialize hyper-parameters.
     epochs = 30
     eta = 0.01
+    hidden_layer_size = 100
 
-    train_network(params, epochs, eta, sigmoid, sigmoid_derivative, training_set, validation_set)
+    # Generate initial weights.
+    params = generate_weights(hidden_layer_size)
+
+    # Train the network.
+    trained_params = train_network(params, epochs, eta, sigmoid, sigmoid_derivative,
+                                   training_set, validation_set)
+
+    # Perform a prediction over the test set, and write it to a file.
+    write_prediction(trained_params, test_x)
+
+
+def write_prediction(trained_params, test_x):
+    """
+    Performs a prediction over the test set and writes it to a file.
+    :param trained_params: trained parameters
+    :param test_x: test set
+    :return: None
+    """
+    with open("test.pred", "w") as test_file:
+        for x in test_x[:-1]:
+
+            # Pass the example through the classifier.
+            output = forward_propagation(x, sigmoid, trained_params)
+
+            # Extract the predicted label.
+            predicted_value = str(np.argmax(output["y_hat"]))
+            test_file.write(predicted_value + '\n')
+
+        # Avoid writing blank line at the end of file.
+        output = forward_propagation(test_x[-1], sigmoid, trained_params)
+        predicted_value = str(np.argmax(output["y_hat"]))
+        test_file.write(predicted_value)
 
 
 def combine_examples(examples, labels):
@@ -51,34 +81,17 @@ def combine_examples(examples, labels):
     return paired_examples
 
 
-def create_weights_matrix(rows, columns=1):
-    """
-    Creates a weights matrix with uniformly distributed values.
-    :param rows: number of rows
-    :param columns: number of columns
-    :return: weights matrix
-    """
-    # TODO change the low and high values, maybe use random instead of uniform
-    weights_matrix = np.empty(shape=(rows, columns))
-
-    for row in xrange(rows):
-        weights_matrix[row, :] = np.random.uniform(-0.08, 0.08, columns)
-
-    return weights_matrix
-
-
-def generate_weights():
+def generate_weights(hidden_layer_size):
     """
     Creates the needed weights for the network.
     :return: dictionary of weights
     """
     input_layer_size = 784
-    hidden_layer_size = 50
     output_layer_size = 10
-    W1 = create_weights_matrix(hidden_layer_size, input_layer_size)
-    b1 = create_weights_matrix(hidden_layer_size)
-    W2 = create_weights_matrix(output_layer_size, hidden_layer_size)
-    b2 = create_weights_matrix(hidden_layer_size)
+    W1 = np.random.uniform(-0.08, 0.08, (hidden_layer_size, input_layer_size))
+    b1 = np.random.uniform(-0.08, 0.08, (hidden_layer_size, 1))
+    W2 = np.random.uniform(-0.08, 0.08, (output_layer_size, hidden_layer_size))
+    b2 = np.random.uniform(-0.08, 0.08, (output_layer_size, 1))
 
     return {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
 
@@ -89,7 +102,7 @@ def softmax(x):
     :param x: input
     :return: output
     """
-    z = np.exp(x - np.amax(x))
+    z = np.exp(x - np.max(x))
     return z / np.sum(z)
 
 
@@ -120,6 +133,7 @@ def forward_propagation(x, activation_function, params):
     :return: output
     """
     W1, b1, W2, b2 = [params[key] for key in ('W1', 'b1', 'W2', 'b2')]
+    x = np.array(x, ndmin=2).T
     z1 = np.dot(W1, x) + b1
     h1 = activation_function(z1)
     z2 = np.dot(W2, h1) + b2
@@ -140,7 +154,7 @@ def one_hot_vector(y, num_of_elements):
     :return: one hot vector
     """
     vector = np.zeros(num_of_elements, dtype=int)
-    vector[y] = 1
+    vector[int(y)] = 1
     return vector
 
 
@@ -153,7 +167,7 @@ def calculate_loss(y, y_hat):
     :return: loss value
     """
     y = one_hot_vector(y, 10)
-    loss = 0
+    loss = 0.0
 
     for i in xrange(y.size):
         loss += y[i] * np.log(y_hat[i])
@@ -169,17 +183,17 @@ def back_propagation(x, y, forward_cache, activation_function_derivative):
     :param activation_function_derivative: activation function derivative
     :return: gradients dictionary
     """
-    W1, W2, h1, y_hat = [forward_cache[key] for key in ('W1', 'W2', 'h1', 'h_hat')]
+    W1, W2, h1, y_hat = [forward_cache[key] for key in ('W1', 'W2', 'h1', 'y_hat')]
 
     # Calculation of the dW2 and db2 gradients.
     # TODO check if global is needed here
     dz2 = y_hat
-    dz2[y] -= 1
-    dW2 = np.outer(dz2, h1)
+    dz2[int(y)] -= 1
+    dW2 = np.outer(dz2, h1.T)
     db2 = dz2
 
     # Calculation of the dW1 and db1 gradients
-    dz1 = np.dot(W2, dz2) * activation_function_derivative(h1)
+    dz1 = np.dot(W2.T, dz2) * activation_function_derivative(h1)
     dW1 = np.outer(dz1, x)
     db1 = dz1
 
@@ -206,9 +220,17 @@ def update(eta, forward_cache, gradients):
 
 
 def predict_on_validation(validation_set, params, activation_function):
+    """
+    Performs a prediction over the validation set.
+    :param validation_set: validation set
+    :param params: parameters dictionary
+    :param activation_function: activation function
+    :return: average loss, accuracy
+    """
     # TODO change variables names, and in general change the code structure
-    good = 0.0
+    correct = 0.0
     sum_loss = 0.0
+    num_of_examples = validation_set.shape[0]
 
     for x, y in validation_set:
         forward_cache = forward_propagation(x, activation_function, params)
@@ -216,16 +238,27 @@ def predict_on_validation(validation_set, params, activation_function):
         sum_loss += loss
 
         if np.argmax(forward_cache["y_hat"]) == y:
-            good += 1
+            correct += 1
 
-    accuracy = good / validation_set[0].shape[0]
-    average_loss = sum_loss / validation_set[0].shape[0]
+    accuracy = correct / num_of_examples
+    average_loss = sum_loss / num_of_examples
 
     return average_loss, accuracy
 
 
 def train_network(params, epochs, eta, activation_function, activation_function_derivative, training_set,
                   validation_set):
+    """
+    Trains the neural network.
+    :param params: weights matrices and vectors
+    :param epochs: number of epochs
+    :param eta: learning rate
+    :param activation_function: activation function
+    :param activation_function_derivative: activation function derivative
+    :param training_set: training set
+    :param validation_set: validation set
+    :return: classifier
+    """
     for e in xrange(epochs):
         sum_loss = 0.0
         np.random.shuffle(training_set)
@@ -239,7 +272,10 @@ def train_network(params, epochs, eta, activation_function, activation_function_
 
         validation_loss, accuracy = predict_on_validation(validation_set, params, activation_function)
 
-        print e, sum_loss / training_set[0].shape[0], validation_loss, "{}%".format(accuracy * 100)
+        print "epoch: {0} train loss: {1} dev loss: {2} accuracy: {3}%".format(e, sum_loss / training_set.shape[0],
+                                                                               validation_loss, accuracy * 100)
+
+    return params
 
 
 if __name__ == "__main__":
